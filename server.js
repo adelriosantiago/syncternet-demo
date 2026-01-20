b372535f858042373c6202cc4bb8fe9bb207f1a9
Alejandro Santiago <adelriosantiago@gmail.com>
2026-01-20 00:03:14 -0500

    Convert to ES module

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const port = 3091
-const express = require("express")
-const bodyParser = require("body-parser")
-const syncternet = require("syncternet")
-const app = express()
+import express from "express"
+import bodyParser from "body-parser"
+import syncternet from "./dev_modules/syncternet/export.js"
 
+const port = 3091
+const app = express()
+ 
 app.use(express.static("static"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
