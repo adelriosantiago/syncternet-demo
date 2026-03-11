6de5328af4c69dd56cc9f581df43aeea064bca6f
Alejandro Santiago <adelriosantiago@gmail.com>
2026-03-11 14:09:10 -0400

    Refactor ecosystem configuration and update start script in package.json

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,15 @@
-const pkg = require('./package.json')
+const pkg = require("./package.json")
 
 module.exports = {
   apps: [
     {
-      name: `${pkg.name}`,
-      script: 'npm',
-      args: 'run start',
+      name: `prod@${pkg.name}`,
+      script: pkg.main,
+      args: "",
+      cwd: __dirname,
+      interpreter: "node",
+      exec_mode: "fork",
+      watch: false,
       instances: 1,
     },
   ],
